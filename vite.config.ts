71abca188731f8a7a95b22579e737a0c3b9c547c
yasirmaxstyle <yasirmu77@gmail.com>
2026-05-14 08:59:59 +0700

    feat: add gh-pages deployment scripts and configure manual chunk splitting for production builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/strange-attractors/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('three')) return 'vendor-three';
+            if (id.includes('react') || id.includes('react-dom')) return 'vendor-react';
+            if (id.includes('@react-three')) return 'vendor-r3f';
+            if (id.includes('@react-three/drei')) return 'vendor-drei';
+            if (id.includes('@react-three/postprocessing')) return 'vendor-postprocessing';
+            if (id.includes('leva')) return 'vendor-leva';
+            return 'vendor-core';
+          }
+        }
+      }
+    }
+  },
 })
